a696c30fc95873dfa916569a36c27e4322aa8245
Bright Carter <brightonwuemeri@gmail.com>
2023-06-14 07:11:20 -0700

    This is a new project

---
+++ script.js
@@ -0,0 +1,7 @@
+var navBar = document.getElementById("navBar");
+    function togglebtn(){
+        navBar.classList.toggle("hidemenu");
+    };
+
+
+// const header = document.getElementById("header");
